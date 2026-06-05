eab4f89f4140a67bed35603577962df2efa00b53
Tapiwanashe Chakurungama <166595427+tapiwanashechakurungama@users.noreply.github.com>
2026-06-05 14:34:34 +0200

    updates

--- settings.js
+++ settings.js
@@ -9,13 +9,13 @@ global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
 global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
+global.botname = '© Nashe-𝐌𝐃' //ur bot name
+global.ownernumber = ['263733517788'] //ur owner number, dont add more than one
+global.ownername = '© Nashe' //ur owner name
 global.websitex = "https://www.youtube.com/@GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "Nashe Inc."
 global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
 global.packname = "Sticker By"
 global.author = "GlobalTechInfo\n\n+923444844060"
